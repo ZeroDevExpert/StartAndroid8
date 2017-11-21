21b96e38c713d85f827798ff4cb1900213853480
user <user@gmail.com>
2017-11-21 02:17:10 +0200

    update Lessons 71

---
+++ p0711_preferencessimple/src/main/java/zerodev/p0711_preferencessimple/MainActivity.java
@@ -0,0 +1,13 @@
+package zerodev.p0711_preferencessimple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
